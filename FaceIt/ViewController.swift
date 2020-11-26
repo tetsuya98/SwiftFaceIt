913286f4704aff3c73aa9afc72883facc0cebdd1
Josselin Abel <josselin1998.abel@gmail.com>
2020-11-26 09:03:53 +0100

    Initial Commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Josselin on 26/11/2020.
+//  Copyright © 2020 Josselin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
