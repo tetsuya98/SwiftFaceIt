8642a1d2e36533d54851d36e2db2c793e4b8e55b
Josselin Abel <josselin1998.abel@gmail.com>
2020-11-26 10:19:47 +0100

    Smiley Eyes

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -11,16 +11,66 @@ import UIKit
 class FaceView: UIView {
 
     var scale: CGFloat = 0.9
+    private var faceRadius: CGFloat {return min(bounds.size.width, bounds.size.height) / 2 * scale}
+    private var faceCenter: CGPoint {return CGPoint(x: bounds.midX, y: bounds.midY)}
+    private var lineWidth: CGFloat = 5.0
+    private var lineColor = UIColor.blue
+    private var eyesOpen: Bool = true
+    private var eyesHappy: Bool = true
+    
+    private func pathForFace() -> UIBezierPath {
+        let path = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        path.lineWidth = lineWidth
+        return path
+    }
+    
+    private enum Eye {
+        case left
+        case right
+    }
+    
+    private struct Ratios {
+        static let faceRadiusToEyeOffset: CGFloat = 2.5
+        static let faceRadiusToEyeRadius: CGFloat = 5
+        static let faceRadiusToMouthOffset: CGFloat = 3
+        static let faceRadiusToMouthWidth: CGFloat = 1
+        static let faceRadiusToMouthHeight: CGFloat = 3
+    }
+    
+    private func pathForEye(_ eye: Eye) -> UIBezierPath {
+        func centerOfEye(_ eye: Eye) -> CGPoint {
+            let eyeOffset = faceRadius / Ratios.faceRadiusToEyeOffset
+            var eyeCenter = faceCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+            return eyeCenter
+        }
+        
+        let eyeRadius = faceRadius / Ratios.faceRadiusToEyeRadius
+        let eyeCenter = centerOfEye(eye)
+        let path: UIBezierPath
+        if eyesOpen {
+            path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
+        } else{
+            if eyesHappy {
+                path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: CGFloat.pi, clockwise: false)
+            } else {
+                path = UIBezierPath()
+                path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
+                path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
+            }
+        }
+        path.lineWidth = lineWidth
+        return path
+    }
+
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        let faceRadius = min(bounds.size.width, bounds.size.height) / 2 * scale
-        let faceCenter = CGPoint(x: bounds.midX, y: bounds.midY)
-        //let faceCenter = convert(center, from: superview)
-        let path = UIBezierPath(arcCenter: faceCenter, radius: faceRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false)
-        path.lineWidth = 5.0
-        UIColor.blue.set()
-        path.stroke()
+        lineColor.set()
+        pathForFace().stroke()
+        pathForEye(Eye.left).stroke()
+        pathForEye(Eye.right).stroke()
     }
     
 
